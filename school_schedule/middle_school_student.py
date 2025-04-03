e69768ae1da263ef05361fd9f714b802a62c8e7d
Viktoriia <lyudkevych.v@gmail.com>
2025-04-03 12:30:28 -0700

    Created a functionsand tests  for MiddleSchoolStudent and all test passes

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -1,3 +1,28 @@
 from .student import Student
 
 # add MiddleSchoolStudent here
+class MiddleSchoolStudent(Student):
+    def __init__(self, name, grade, classes, gets_transportation=False, clubs=None):
+        super().__init__(name, grade, classes)
+        self.gets_transportation = gets_transportation
+        self.clubs = clubs if clubs is not None else []
+
+    def display_get_transportation_message(self):
+        if self.gets_transportation:
+            return "This student receives transportation."
+        else:
+            return "This student does not receive transportation."
+
+    def display_clubs(self):
+        club_str = ", ".join(self.clubs)
+
+        if club_str:
+           return f"{self.name} is a member of: {club_str}"
+    
+        return f"{self.name} hasn't joined a club yet."
+
+    
+    def summary(self):
+        student_summary = super().summary()
+        transportation_message = self.display_get_transportation_message()
+        return "\n".join((student_summary, transportation_message))
