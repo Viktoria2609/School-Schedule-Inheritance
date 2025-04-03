e4497633403ec546c35963fd8ae31bebd33aa947
Viktoriia <lyudkevych.v@gmail.com>
2025-04-03 13:37:06 -0700

    Deleted club func and atributte

--- school_schedule/middle_school_student.py
+++ school_schedule/middle_school_student.py
@@ -2,24 +2,15 @@ from .student import Student
 
 # add MiddleSchoolStudent here
 class MiddleSchoolStudent(Student):
-    def __init__(self, name, grade, classes, gets_transportation = False, clubs = None):
+    def __init__(self, name, grade, classes, gets_transportation = False):
         super().__init__(name, grade, classes)
         self.gets_transportation = gets_transportation
-        self.clubs = clubs if clubs is not None else []
 
     def display_get_transportation_message(self):
         if self.gets_transportation:
             return "This student receives transportation."
         else:
             return "This student does not receive transportation."
-
-    def display_clubs(self):
-        club_str = ", ".join(self.clubs)
-
-        if club_str:
-           return f"{self.name} is a member of: {club_str}"
-    
-        return f"{self.name} hasn't joined a club yet."
 
     
     def summary(self):
